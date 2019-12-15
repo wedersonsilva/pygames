92e49b7442b5697a353e1984bf61adcc2a810492
Wederson Silva <wederson18@gmail.com>
2019-12-15 19:22:02 -0300

    It runs only grid successfully

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -37,10 +37,23 @@ class snake(object):
 
 
 def drawGrid(w, rows, surface):
-    pass
+    sizeBtwn = w // rows
+
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
+        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
+
 
 def redrawWindow(surface):
-    pass
+    global rows, width
+    surface.fill((0, 0, 0))
+    drawGrid(width, rows, surface)
+    pygame.display.update()
 
 def randomSnack(rown, items):
     pass
@@ -50,14 +63,21 @@ def message_box(subject, content):
 
 
 def main():
+    global rows, width
+    width = 500
+    rows = 20
+    win = pygame.display.set_mode((width, width))
+    s = snake((255, 0, 0), (10, 10))
+    flag = True
+
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        redrawWindow(win)
     pass
 
 
-rows =
-w = 
-h = 
-
-cube.rows = rows
-cube.w = w
 
 main()
